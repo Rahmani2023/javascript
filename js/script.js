8b580f6b50f61bd32e09a8760a8427ecf838cd17
Rahmani2023 <136079306+Rahmani2023@users.noreply.github.com>
2023-07-02 17:02:50 +0200

    add objects to pokemonList delete not wanted html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-const pokemonList = [];
+const pokemonList = [
+  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+  { name: "Fearow", height: 1.2, types: ["flying", "normal"] },
+  { name: "Ninetales", height: 1.1, types: ["fire"] },
+];
