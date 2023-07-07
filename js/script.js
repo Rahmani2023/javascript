cfaa5fbf7ffaecf5119e2287b633d10411a60414
Rahmani2023 <136079306+Rahmani2023@users.noreply.github.com>
2023-07-07 12:32:12 +0200

    add log

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ const pokemonList = [
   { name: "Fearow", height: 1.2, types: ["flying", "normal"] },
   { name: "Ninetales", height: 1.1, types: ["fire"] },
 ];
+
+let text ="charizard";
+for (let x in pokemonList) {
+  Text += person[x];
+
+  charizard(height : 1.7)
+}
+let charizard = true;
+if (charizard) {
+  console.log('-Wow,thats Big!');
+}
+Charizard(height: 1.7)
+Fearow(height: 1.2)
+Ninetales(height: 1.1)
