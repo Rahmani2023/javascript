60611b6e9b94e646c1c59e0bbb05d43f5941c02c
Rahmani2023 <136079306+Rahmani2023@users.noreply.github.com>
2023-07-13 20:39:30 +0200

    add element

--- js/script.js
+++ js/script.js
@@ -11,9 +11,13 @@ for (let x in pokemonList) {
   charizard(height : 1.7)
 }
 let charizard = true;
-if (charizard) {
+if (charizard [i].height >=1) {
   console.log('-Wow,thats Big!');
 }
 Charizard(height: 1.7)
 Fearow(height: 1.2)
 Ninetales(height: 1.1)
+
+for (let i = 0;  i < pokemonList.length; i++) {
+  // for-loop body
+  }
