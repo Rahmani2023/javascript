bead79d4344a2c98a8c8e15c5f778c00ff92a12f
Rahmani2023 <136079306+Rahmani2023@users.noreply.github.com>
2023-07-14 23:15:32 +0200

    add function

--- js/script.js
+++ js/script.js
@@ -16,4 +16,25 @@ if (charizard [].height >=1) {
 for (let i = 0;  i < pokemonList.length; i++) {
  
   }
+
+
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      return "Not Allowed";
+    }
+    return a / b;
+  }
+  
   
